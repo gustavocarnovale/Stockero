8e3acbe1d8ec17c75052ee14fd446136b020c657
gustavocarnovale <72949562+gustavocarnovale@users.noreply.github.com>
2024-01-11 17:19:30 -0300

    Add files via upload

--- app/app.py
+++ app/app.py
@@ -12,6 +12,10 @@ firebase_sdk = credentials.Certificate('stockero-40e02-firebase-adminsdk-zi40z-3
 
 firebase_admin.initialize_app(firebase_sdk,{'databaseURL': 'https://stockero-40e02-default-rtdb.firebaseio.com' })
 
+fecha = datetime.datetime.now()
+fecha_str = fecha.strftime("%Y-%m-%d")
+id = fecha.strftime("%H:%M:%S")
+
 @app.route('/')
 def index():
 
@@ -19,18 +23,36 @@ def index():
 
     return render_template('index.html', data = data)
 
+@app.route('/ventas', methods=["GET"])
+def ventas():
+
+    productos = fnCargaProductos("ekos")
+    producto_seleccionado = request.args.get('producto')
+    
+    if producto_seleccionado:
+        return mostrar_ventas(producto_seleccionado)
+
+    return render_template('ventas.html', productos = productos)
+
+@app.route('/ventas/<producto>', methods = ["GET"])
+def mostrar_ventas(producto_seleccionado):
+
+    ruta_nodo_ventas = f'/ventas/{producto_seleccionado}'
+    nodo_referencia_ventas = db.reference(ruta_nodo_ventas)
+    lista_ventas = nodo_referencia_ventas.get()
+
+    return render_template('listaVentas.html', ventas=lista_ventas)
+
 @app.route('/stock')
 def stock():
 
-    ruta_nodo = f'/productos/'
-    nodo_referencia = db.reference(ruta_nodo)
-    datos = nodo_referencia.get()
+    datos = fnCargaProductos("")
 
     return render_template('stock.html', datos=datos)
 
 @app.route('/vender')
 def vender():
-
+    
     return render_template('vender.html')
 
 @app.route('/vender', methods =['POST'])
@@ -39,13 +61,13 @@ def fnVendeProducto():
     data = {
         "nombre":request.form['nombre'],
         "linea": request.form['linea'],
-        "cantidad": int(request.form['cantidad_vendida'])
+        "cantidad": int(request.form['cantidad_vendida']),
+        "precio":  float(request.form['precio'])
     }
 
-    data_lower = fnDataLower(data)
+    data_lower = fnLower(data)
     fnOperacionProducto(data_lower, op)
     fnRegistraVenta(data_lower)
-
     return render_template('vender.html')
 
 @app.route('/producto')
@@ -59,69 +81,76 @@ def fnGuardaProducto():
     data = {
         "nombre":request.form['nombre'],
         "linea": request.form['linea'],
-        "cantidad": int(request.form['cantidad'])
+        "cantidad": int(request.form['cantidad']),
+        "precio": float(request.form['precio'])
     }
 
-    data_lower = fnDataLower(data)
-    fnOperacionProducto(data_lower, op)
-
+    dataLower = fnLower(data)
+    fnOperacionProducto(dataLower, op)
 
     return render_template('producto.html')
 
-def fnRegistraVenta(data_lower):
-    fecha = datetime.datetime.now()
-    fecha_str = fecha.strftime("%Y-%m-%d %H:%M:%S")
-    datos = {
-    'cantidad': data_lower['cantidad'],
-    'fecha': fecha_str,
-    'precio': 0 
-    }
-    ruta_nodo = f'/ventas/{data_lower["linea"]}/{data_lower["nombre"]}'
-    nodo_referencia = db.reference(ruta_nodo)
+#FUNCIONES#    
+#######################################################################################################################################
+def fnCargaProductos(linea):
     
-    lista_ventas = nodo_referencia.get()
-    lista_ventas.append(datos)
-    #nodo_referencia.set(lista_ventas)
-    print(lista_ventas)
+    ruta_nodo = f'/productos/{linea}'
+    nodo_referencia = db.reference(ruta_nodo)
+    datos = nodo_referencia.get()
+    print(datos)
+    return datos
+
+#######################################################################################################################################
+def fnRegistraVenta(dataLower):
+    
+    datos = {
+        "cantidad": dataLower['cantidad'],
+        "precio": dataLower['precio']
+    }
+
+    ruta_nodo = f'/ventas/{dataLower["nombre"]}/{id}'
+    nodo_referencia = db.reference(ruta_nodo)
+    dato = nodo_referencia.get()
+    print(dato)
+    nodo_referencia.set(datos)
 
     return
 
-def fnOperacionProducto(data_lower, op):
+#######################################################################################################################################
+def fnOperacionProducto(dataLower, op):
     
-    ruta_nodo = f'/productos/{data_lower["linea"]}/{data_lower["nombre"]}'
+    ruta_nodo = f'/productos/{dataLower["linea"]}/{dataLower["nombre"]}'
     nodo_referencia = db.reference(ruta_nodo)
 
+    datosGuardado = {
+        "cantidad":dataLower['cantidad'],
+        "precio": dataLower['precio']
+    }
     if nodo_referencia.get():
 
         cantidad_actual = nodo_referencia.child('cantidad').get() or 0
-        opResta = cantidad_actual - int(data_lower["cantidad"]) if cantidad_actual > int(data_lower["cantidad"]) and op == 0 else 0
-        opSuma = cantidad_actual + int(data_lower["cantidad"]) if  op == 1 else opResta
+        opResta = cantidad_actual - int(dataLower["cantidad"]) if cantidad_actual > int(dataLower["cantidad"]) and op == 0 else 0
+        opSuma = cantidad_actual + int(dataLower["cantidad"]) if  op == 1 else opResta
         nueva_cantidad = opSuma
         nodo_referencia.update({'cantidad': nueva_cantidad})
 
     else:
-        nodo_referencia.set(data_lower)
+        nodo_referencia.set(datosGuardado)
     return    
 
-
-def fnDataLower(data):
+#######################################################################################################################################
+def fnLower(data):
 
     data =  {key: value.lower() if isinstance(value, str) else value for key, value in data.items()}
 
     return data
 
-def query():
-
-    print(request.args.get('param1')) 
-
-    return "Listo!"
-
+#######################################################################################################################################
 def pag(error):
     
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
 
-    app.add_url_rule('/query', view_func = query )
     app.register_error_handler(404, pag)
     app.run(debug=True)
